210b1786051c812112f5f31e7ae05f35c5059efc
johnson-abraham <johnson.thomson@anywhere.co>
2020-01-19 13:41:19 +0530

    reversed int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,12 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+/**
+ * 
+ * @param {Number} n 
+ */
+function reverseInt(n) {
+    return Number.parseInt(n.toString().split('').reverse().join('')) * Math.sign(n);
+}
 
 module.exports = reverseInt;
